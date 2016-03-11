4366deabf94bb31aedca34b9d06e1b873b676ebe
Leticia Maia <leticiamaiat@gmail.com>
2016-03-10 22:11:23 -0800

    Hangman Part 1 Finished - Guessing letters working

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,15 +10,55 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var underscoresLabel: UILabel!
+    var gamePhrase: String?
+    var guessedLetters = [Character]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
+        self.gamePhrase = phrase
+
+        updateUnderscoresLabelText()
         print(phrase)
     }
+    
+    @IBAction func didGuess(sender: UIButton) {
+        let letterGuess = sender.currentTitle
+        sender.enabled = false
+        if self.gamePhrase!.characters.contains(Character(letterGuess!)) {
+            removeUnderscores(letterGuess!)
+        }
+    }
+    
+    func removeUnderscores(letterGuess: String) {
+        guessedLetters.append(Character(letterGuess))
+        updateUnderscoresLabelText()
 
+    }
+    
+    func updateUnderscoresLabelText() {
+        var labelText = ""
+        
+        for letter in gamePhrase!.characters {
+            if letter != " " {
+                if self.guessedLetters.contains(letter) {
+                    labelText += String(letter)
+                    labelText += "  "
+                } else {
+                    labelText += "_  "
+                }
+            } else {
+                labelText += "  "
+            }
+            
+        }
+        underscoresLabel.text = labelText
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
